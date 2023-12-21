186ce6d093b7d170bd92b8d57df11fa9cef131eb
ranpy13 <ranpy13@github.com>
2023-12-21 20:38:25 +0530

    bullets added

--- summ-two.py
+++ summ-two.py
@@ -6,16 +6,19 @@ def summarize(text, language="english", sentences_count=5):
     parser = PlaintextParser.from_string(text, Tokenizer(language))
     summarizer = LsaSummarizer()
     summary = summarizer(parser.document, sentences_count)
-    return ' '.join([str(sentence) for sentence in summary])
+    # return ' '.join([str(sentence) for sentence in summary])
+    return [str(sentence) for sentence in summary]
 
 if __name__ == "__main__":
     with open('dump.txt','r',encoding='utf-8') as inp_file:
         data = inp_file.read()
     text = data
-    summary = summarize(text)
+    count = max(text.count(' ')//100, 5)
+    summary = summarize(text, sentences_count=count)
     # print(summary)
     print("Generating Summary... ", end="")
     with open('summary.txt','w') as res:
-        res.write(summary)
+        for points in summary:
+            res.write("-\t"+points+"\n")
         res.close()
     print("Generated!")
